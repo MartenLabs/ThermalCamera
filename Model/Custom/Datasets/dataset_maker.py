66656821696d0ed46082315730adfd445ab77d8f
charcoalSkies <jun48470@gmail.com>
2023-11-02 02:17:56 +0900

    feat: 순차 데이터셋 생성 코드 (Yolo5 데이터셋)

--- Model/Custom/Datasets/dataset_maker.py
+++ Model/Custom/Datasets/dataset_maker.py
@@ -4,6 +4,8 @@ import cv2
 import os
 import re
 
+# COOC
+
 def get_image_id_from_filename(filename, annotations):
     for img in annotations['images']:
         if img['file_name'] == filename:
@@ -12,18 +14,23 @@ def get_image_id_from_filename(filename, annotations):
 
 def load_masks_and_number(image_id, annotations):
     masks = []
+    number_of_people = 0
+    
     for ann in annotations['annotations']:
         if ann['image_id'] == image_id:
+            if ann['category_id'] > number_of_people:
+                number_of_people = ann['category_id']
+
             segmentation = ann['segmentation'][0]
             img_info = next(filter(lambda x: x['id'] == image_id, annotations['images']))
             mask = np.zeros((img_info['height'], img_info['width']), dtype=np.uint8)
             cv2.fillPoly(mask, [np.array(segmentation).reshape(-1, 2).astype(np.int32)], 255)
             masks.append(mask)
-    if len(masks) > 0:
+
+    if masks:
         combined_mask = np.max(np.stack(masks, axis=0), axis=0)
-        return combined_mask, len(masks) - 1  # subtract one to get number of people
-    else:
-        return None, None
+        return combined_mask, number_of_people
+    return None, None
 
 def process_images_and_save(input_folder, annotation_file, prefix):
     with open(annotation_file, 'r') as f:
@@ -48,10 +55,12 @@ def process_images_and_save(input_folder, annotation_file, prefix):
         original_image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
         mask, number = load_masks_and_number(get_image_id_from_filename(image_file, annotations), annotations)
 
-        if mask is None or number is None:
-            continue
+        if mask is not None:
+            filtered_image = cv2.bitwise_and(original_image, original_image, mask=mask)
+        else:
+            filtered_image = np.zeros_like(original_image)
+            number = 0
 
-        filtered_image = cv2.bitwise_and(original_image, original_image, mask=mask)
         images.append(original_image)
         filters.append(filtered_image)
         numbers.append(number)
