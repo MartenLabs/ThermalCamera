63d93b4cef534274758c102c875148242a985e8f
charcoalSkies <jun48470@gmail.com>
2023-11-05 23:01:22 +0900

    feat: 최대 4명 데이터셋 mp4 to png

--- Model/Custom/Datasets/getFrame.py
+++ Model/Custom/Datasets/getFrame.py
@@ -6,13 +6,17 @@ get_location = '/Users/mac/Dev/Project/ThermalCamera/Model/Custom/Datasets/origi
 
 img_count:int = 0
 # 해당 경로에 있는 모든 mp4 파일 경로를 리스트로 저장
-file_list = [file for file in os.listdir(get_location) if not file.startswith('.')]
+file_list = [file for file in os.listdir(get_location) if not file.startswith('.mp4')]
 
 # for문을 통해 모든 mp4 파일을 돌면서 이미지 추출
 for file in file_list:
+    
+    base_filename = os.path.splitext(file)[0] 
+    save_directory = os.path.join(get_location, base_filename)
+
     # 비디오 이름으로 폴더 생성 후 경로 저장 
     try:
-        os.mkdir(f"/Users/mac/Dev/Project/ThermalCamera/Model/Custom/Datasets/original/Video/img/{file}")
+        os.mkdir(save_directory)
     except FileExistsError:
         pass 
     vidcap = cv2.VideoCapture(get_location + file)
@@ -22,11 +26,11 @@ for file in file_list:
         # 더 이상 프레임을 읽을 수 없을 때 루프 종료
         if not ret:
             break
-        # 5프레임당 하나씩 이미지 추출
+        # 1프레임당 하나씩 이미지 추출
         if int(vidcap.get(1)) % 1 == 0:
             print('Saved frame number : ' + str(int(vidcap.get(1))))
             # 추출된 이미지가 저장되는 경로
-            cv2.imwrite(f"/Users/mac/Dev/Project/ThermalCamera/Model/Custom/Datasets/original/Video/img/{count}_png.png", image)
+            cv2.imwrite(f"{save_directory}/{count}_png.png", image)
         count += 1
         img_count += 1
 
