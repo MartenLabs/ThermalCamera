aa0baf9c467175e4104304d2bbff17eeeabfdde8
Greyhound <jun48470@gmail.com>
2024-03-30 23:36:28 +0900

    feat : retinaNet Test

---
+++ RaspberryPi/Backup/Project/ThermalCamera/main/test.py
@@ -0,0 +1,69 @@
+
+import time
+import board
+import busio
+import numpy as np
+from scipy import ndimage, interpolate
+import adafruit_mlx90640
+import cv2
+import datetime
+import tensorflow as tf
+
+from tensorflow import keras
+
+# import keras_retinanet
+from keras_retinanet import models
+from keras_retinanet.utils.image import read_image_bgr, preprocess_image, resize_image
+from keras_retinanet.utils.visualization import draw_box, draw_caption
+from keras_retinanet.utils.colors import label_color
+from keras_retinanet.utils.gpu import setup_gpu
+
+# import miscellaneous modules
+import matplotlib.pyplot as plt
+import cv2
+import os
+import numpy as np
+import time
+
+model_path = 'main.h5'
+model = models.load_model(model_path, backbone_name='resnet50')
+labels_to_names = {0: 'person'}
+
+
+image = read_image_bgr('14_jpg.rf.eb2e3b1fa821284bc0b8152e7dccaa4c.jpg')
+print("Image shape:", image.shape)
+
+# copy to draw on
+draw = image.copy()
+draw = cv2.cvtColor(draw, cv2.COLOR_BGR2RGB)
+
+# preprocess image for network
+image = preprocess_image(image)
+image, scale = resize_image(image)
+
+# process image
+start = time.time()
+boxes, scores, labels = model.predict_on_batch(np.expand_dims(image, axis=0))
+print("processing time: ", time.time() - start)
+
+# correct for image scale
+boxes /= scale
+
+# visualize detections
+for box, score, label in zip(boxes[0], scores[0], labels[0]):
+    # scores are sorted so we can break
+    if score < 0.5:
+        break
+        
+    color = label_color(label)
+    
+    b = box.astype(int)
+    draw_box(draw, b, color=color)
+    
+    caption = "{} {:.3f}".format(labels_to_names[label], score)
+    draw_caption(draw, b, caption)
+    
+plt.figure(figsize=(15, 15))
+plt.axis('off')
+plt.imshow(draw)
+plt.show()
